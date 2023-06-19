e368081273ab09b639ffe08ef60f86b5dc4361d0
sureshrtech1 <135309412+sureshrtech1@users.noreply.github.com>
2023-06-20 00:19:20 +0530

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -3,6 +3,5 @@ def lambda_handler(event, context):
     "statusCode": 200,
     "body": {
       "access_token": "7e2ffd17-af45-4f3c-8d93-ab2843784054:sn"
-      "Access-Control-Allow-Origin": '*'
     }
   }
