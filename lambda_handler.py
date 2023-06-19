060df2680117ad276de8c53e12bdf4b7ac046dc7
sureshrtech1 <135309412+sureshrtech1@users.noreply.github.com>
2023-06-19 23:42:44 +0530

    Create lambda_handler.py

---
+++ lambda_handler.py
@@ -0,0 +1,8 @@
+def lambda_handler(event, context):
+  return {
+    "statusCode": 200,
+    "body": {
+      "access_token": "7e2ffd17-af45-4f3c-8d93-ab2843784054:sn"
+      "Access-Control-Allow-Origin": '*'
+    }
+  }
