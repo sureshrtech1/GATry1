ea309f0daa6ecf3e0c0a0544fc6bc2e5e530ce50
sureshrtech1 <135309412+sureshrtech1@users.noreply.github.com>
2023-06-20 00:35:35 +0530

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,6 +2,6 @@ def lambda_handler(event, context):
   return {
     "statusCode": 200,
     "body": {
-      "access_token": "7e2ffd17-af45-4f3c-8d93-ab2843784054:sn"
+      "access_token": "09766be9-6923-4a42-b810-527efb3465b2:sn"
     }
   }
