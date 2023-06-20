9653fd989419bfb3caa85a4e87af79eed56b0848
sureshrtech1 <135309412+sureshrtech1@users.noreply.github.com>
2023-06-20 20:16:17 +0530

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,7 +1,11 @@
+import requests
+
+url = 'https://j0dulj4ywh.execute-api.ap-southeast-2.amazonaws.com/default/MSyncForContenful';
+headers = {
+                    'X-Api-Key': '6bdxyzkVgKAj9jrBPO9e2PvR73qCg2t9zwtFMYH8',
+                    'access-control-allow-origin': '*',
+                    'origin': 'https://app.contentful.com',
+                    'Content-Type': 'application/json'
+                }
 def lambda_handler(event, context):
-  return {
-    "statusCode": 200,
-    "body": {
-      "access_token": "09766be9-6923-4a42-b810-527efb3465b2:sn"
-    }
-  }
+  return requests.get(url, headers=headers);
